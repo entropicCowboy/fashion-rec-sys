9949f84a1db3de188fc09128ff7cfc2989253f82
Frija Reinert <frija.reinert@gmail.com>
2023-12-06 16:26:23 -0600

    added in Sam's scraped photos

--- image_scraping/pin_stylescraper.py
+++ image_scraping/pin_stylescraper.py
@@ -4,15 +4,15 @@ import json
 f = open('base_style_data.json')
 data = json.load(f)
 f.close()
-for style in data["styles"][300:300]:
-    style_name = style["name"]
-    print(style_name + "-fashion")
-    details = pinscrape.scraper.scrape(style_name + "fashion-clothing", "style_images/" + style_name, {}, 10, 20)
-    if not details["isDownloaded"]:
-        print("\n\n**ERROR IN DOWNLOADING STYLE**\n\n")
+# for style in data["styles"][300:300]:
+#     style_name = style["name"]
+#     print(style_name + "-fashion")
+#     details = pinscrape.scraper.scrape(style_name + "fashion-clothing", "style_images/" + style_name, {}, 10, 20)
+#     if not details["isDownloaded"]:
+#         print("\n\n**ERROR IN DOWNLOADING STYLE**\n\n")
 
 # style_name = ""
 # print(style_name + "-fashion")
-# details = pinscrape.scraper.scrape(style_name + "fashion-clothing", "style_images/" + style_name, {}, 10, 20)
+# details = pinscrape.scraper.scrape(style_name + "fashion-clothing", "sam_style_images/Worked/" + style_name, {}, 10, 20)
 # if not details["isDownloaded"]:
 #     print("\n\n**ERROR IN DOWNLOADING STYLE**\n\n")
